858705253f3fc16a0c3dfd9e352cd508b52229ea
Luis Daniel <Luis Daniel@LuisDaniel.redes.itver.com>
2016-09-05 13:44:24 -0500

    METODOS CRUD 05/09/2016

--- basic/config/db.php
+++ basic/config/db.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=dbyii',
+    'dsn' => 'mysql:host=localhost;dbname=dbyiicrud',
     'username' => 'root',
     'password' => 'password',
     'charset' => 'utf8',
---
+++ basic/models/Usuario.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "usuario".
+ *
+ * @property integer $id
+ * @property string $username
+ * @property string $type
+ * @property string $descripcion
+ */
+class Usuario extends \yii\db\ActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'usuario';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['username', 'type', 'descripcion'], 'required'],
+            [['username', 'type', 'descripcion'], 'string', 'max' => 45],
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'username' => 'Username',
+            'type' => 'Type',
+            'descripcion' => 'Descripcion',
+        ];
+    }
+}
